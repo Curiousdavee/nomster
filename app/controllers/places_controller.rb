acc73f4496cf5d5e936cf5fc8eeb5ac9d32ddcbb
david chu <davidchu334@gmail.com>
2017-01-23 01:19:39 +0000

    added total entriesusing will_pagination gem

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
-    @posts = Place.all.paginate(:page => params[:page], :per_page => 10)
+    @posts = Place.all.paginate(:page => params[:page], :total_entries => 100, :per_page => 10)
     @places_last = Place.last
   end
 end
