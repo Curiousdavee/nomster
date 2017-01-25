39e19a24fb8402733dd081b3cf43a4c682e1c965
david chu <davidchu334@gmail.com>
2017-01-25 17:57:14 +0000

    adjusted pagnation size, show correct 10 entries per page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,13 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
-    @posts = Place.all.paginate(:page => params[:page], :total_entries => 100, :per_page => 10)
+    #@places = Place.all
+    @places = Place.all.paginate(:page => params[:page], :per_page => 10)
     @places_last = Place.last
   end
+
+  def new
+
+  end
+
 end
