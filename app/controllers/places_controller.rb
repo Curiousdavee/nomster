0799adb1a6db717909733efce083dd41869e1e12
david chu <davidchu334@gmail.com>
2017-01-28 20:35:24 +0000

    edit function enables

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -26,6 +26,11 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
 
   private
   # anything below, this is something that can be used
