5f62cffd630a9a15a37a718619ab23b12bf12ea0
david chu <davidchu334@gmail.com>
2017-01-31 03:33:05 +0000

    added logic: only long-in user who created a place can delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   # we want to require a user to be logged in only for the new and create actions
 
   def index
@@ -44,6 +44,10 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
     @place.destroy
     redirect_to root_path
 
