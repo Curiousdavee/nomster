7d646c720202ba72700481e2285d046cd730b15e
david chu <davidchu334@gmail.com>
2017-02-07 17:35:21 +0000

    Added model to create comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,14 +3,15 @@ class Comment < ActiveRecord::Base
   belongs_to :place
 
   RATINGS = {
-    'one star'     => '1_star',
-    'two stars'    => '2_star',
-    'three stars'  => '3_star',
-    'four stars'   => '4_star',
-    'five stars'   => '5_star'
+    'one star'  => '1_star',
+    'two stars' => '2_stars',
+    'three stars' => '3_stars',
+    'four stars' => '4_stars',
+    'five stars' => '5_stars'
   }
 
   def humanized_rating
     RATINGS.invert[self.rating]
   end
+
 end
