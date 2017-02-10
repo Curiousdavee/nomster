579069be7f2cbd77915e4365c001450149b2e134
david chu <davidchu334@gmail.com>
2017-02-10 21:34:01 +0000

    fixed ENV issue related to Amazon

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "exmaple.com",
+    domain: "example.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_ADDRESS'],
