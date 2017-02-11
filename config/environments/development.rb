06f9a320c596bb7c68fd4bd9ebd09ef5de1bb66d
david chu <davidchu334@gmail.com>
2017-02-11 05:39:56 +0000

    adjusting emailer to send to the actual user who created the place

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   }
 
   # Device Gem setup
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = {host: 'localhost:3000'}
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
