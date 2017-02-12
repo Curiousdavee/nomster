8f700dcb21b1782f652c34f2dca4afa02b06e07c
david chu <davidchu334@gmail.com>
2017-02-12 05:49:47 +0000

    display created places on profile page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,5 +18,6 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:message, :rating)
   end
+
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
   end
+
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.comments.destroy(comment_params.merge(user: current_user))
+  #   redirect_to user_path(@user)
+  # end
+
+
+  # private
+
+  # def comment_params
+  #   params.require(:comment).permit(:message, :rating)
+  # end
 
 end
