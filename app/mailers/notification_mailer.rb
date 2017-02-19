5b35331f2dbe30aea14249f206a422c39712b0e1
david chu <davidchu334@gmail.com>
2017-02-19 17:18:20 +0000

    fixed adding a comment issue

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,8 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added(comment) #object
-    # @place = comment.place
-    # why do i need to use an argument
-    # so in this case, argument is indicating Comment db?
-    # @place = Comment.place #class.model - no information
+  def comment_added(comment)
+    @place = comment.place
     @place_owner = @place.user
     mail(to: @place_owner.email,
          subject: "A comment has been added to #{@place.name}")
